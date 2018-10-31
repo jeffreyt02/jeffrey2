d8fff152577ef267dc4eb3ad2f4c02a326732cf0
jeffreyt02 <jeffreytt02@gmail.com>
2018-10-31 11:09:28 +0100

    je 2de eerste commit

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <p>dubbel check of je het nog allemaal kan kerel</p>
           <a
             className="App-link"
             href="https://reactjs.org"
